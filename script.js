584e3a4625612cbd51ba43bc108a6e079998046d
sixtysixgames <nosbig.kram@hotmail.co.uk>
2021-08-21 13:56:38 +0000

    added rand function

--- script.js
+++ script.js
@@ -19,4 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     Author     : sixtysixgames
 */
 
-
+function rand(i){
+    ret = Math.random() * i;
+    ret = Math.round(ret);
+    return ret;
+}
